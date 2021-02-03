6315f70518effe69732e1566ac9435de3c19c457
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-03 22:53:48 +0000

    ran linter on f(x)

--- countOnly.js
+++ countOnly.js
@@ -11,18 +11,18 @@ const assertEqual = function(actual, expected) {
 };
 
 
-// 
+// countOnly counts the occurences of a string w/in an array
 
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
   for (let item of allItems) {
-    if (itemsToCount[item]){
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
         results[item] = 1;
       }
-    }  
+    }
   }
   return results;
 };
@@ -32,7 +32,7 @@ const countOnly = function(allItems, itemsToCount) {
 
 const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
 
-const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
