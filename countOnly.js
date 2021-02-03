f2c1309f35eedda16d0add6c775322a2a3358b16
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-03 22:52:18 +0000

    createt f(x) to count the occurences of a string w/in an array

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+// assertation f(x)
+
+const assertEqual = function(actual, expected) {
+  const pass = "🥰 🎉 Assertation Passed: ";
+  const fail = "😭 😱 Assertation Failed: ";
+  if (actual === expected) {
+    console.log(`${pass} ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} ${actual} !== ${expected}`);
+  }
+};
+
+
+// 
+
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+  for (let item of allItems) {
+    if (itemsToCount[item]){
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }  
+  }
+  return results;
+};
+
+
+// test code
+
+const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
