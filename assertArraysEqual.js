59d48d2c607854e9f683c079d9e92fa9590055c6
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-03 04:57:07 +0000

    created funtion to compare arrays and display a message if they are/are not equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = function(arrayUno, arrayDeux) {
+  const pass = "🥰 🎉 Assertation Passed: ";
+  const fail = "😭 😱 Assertation Failed: ";
+  if (eqArrays(arrayUno, arrayDeux)) {
+    console.log(`${pass} ${arrayUno} === ${arrayDeux}`);
+  } else {
+    console.log(`${fail} ${arrayUno} !== ${arrayDeux}`);
+  }
+
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+// test code
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //pass
+assertArraysEqual([1, 2], [3, 5]); //fail
