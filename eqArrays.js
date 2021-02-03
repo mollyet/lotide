8886eb6593a8615594bbe6325c08921eda4083b2
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-03 04:56:37 +0000

    updated test case to check against mismatched types

--- eqArrays.js
+++ eqArrays.js
@@ -28,4 +28,4 @@ const eqArrays = function(arrOne, arrTwo) {
 // test code to test it within assertEqual
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // this is the same array
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // order should match
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false); //arrays with diff lenghts don't match
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false); //arrays with diff lenghts don't match
