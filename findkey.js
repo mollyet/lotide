ffbb8e19b200fbfaa6c7a58552f952f613196160
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-05 23:06:45 +0000

    refactored findKeyByValue to find a key with a callback f(x)

---
+++ findkey.js
@@ -0,0 +1,35 @@
+// assertation f(x)
+
+const assertEqual = function(actual, expected) {
+  const pass = "🥰 🎉 Assertation Passed: ";
+  const fail = "😭 😱 Assertation Failed: ";
+  if (actual === expected) {
+    console.log(`${pass} ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} ${actual} !== ${expected}`);
+  }
+};
+
+
+///
+const findKey = function(obj, callback) {
+  for (let key of Object.keys(obj)) {
+    if (callback(obj[key]) === true) {
+      return key;
+    }
+  }
+
+};
+
+//test code
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(result1, "noma");
