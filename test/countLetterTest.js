f784c1a417effee5c927d6175052219ac83b9d35
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-09 02:04:11 +0000

    refactored countLetters to work with unit testing, created unit tests for countLetters

---
+++ test/countLetterTest.js
@@ -0,0 +1,30 @@
+const assert = require("chai").assert;
+const countLetters = require("../countLetters");
+
+
+//
+
+describe("#countLetters", () => {
+  const words = "lighthouse in the house";
+  const result = {
+    l: 1,
+    i: 2,
+    g: 1,
+    h: 4,
+    t: 2,
+    o: 2,
+    u: 2,
+    s: 2,
+    e: 3,
+    n: 1,
+  };
+  it("should return a count of letters in a string as an object", () => {
+    assert.deepEqual(countLetters(words), result);
+  });
+  it("should not count spaces", () => {
+    assert.strictEqual(result[" "], undefined);
+  });
+  it("should return undefined for leters that are not the original string", () => {
+    assert.strictEqual(result["z"], undefined);
+  });
+});
