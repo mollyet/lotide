81ee34dec625ece27836849f972a0c1165ce0feb
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-09 03:25:41 +0000

    refactored && implemented unit testing for countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,16 +1,3 @@
-// assertation f(x)
-
-const assertEqual = function(actual, expected) {
-  const pass = "🥰 🎉 Assertation Passed: ";
-  const fail = "😭 😱 Assertation Failed: ";
-  if (actual === expected) {
-    console.log(`${pass} ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} ${actual} !== ${expected}`);
-  }
-};
-
-
 // countOnly counts the occurences of a string w/in an array
 
 const countOnly = function(allItems, itemsToCount) {
@@ -28,14 +15,8 @@ const countOnly = function(allItems, itemsToCount) {
 };
 
 
-// test code
+// 
 
-const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+module.exports = countOnly;
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
 
