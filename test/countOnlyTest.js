007cd92c559bfb6cb2b4274e3a0a5f527193c531
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-09 03:45:31 +0000

    linted!

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -7,16 +7,6 @@ describe("#countOnly", () => {
   const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
   const result = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
   it("it counts the occurances of a string within an array", () => {
-    assert.equal(result["Fang"], 2)
+    assert.equal(result["Fang"], 2);
   });
 });
-
-
-// const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agohanna": false });
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
