c18004e000f6da39db1f24ddb2512f65a52f0d80
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-09 02:05:43 +0000

    refactored countLetters for unit testing, implemented unit testing for countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,5 @@
-// assertation function
-
-const assertEqual = function(actual, expected) {
-  const pass = "🥰 🎉 Assertation Passed: ";
-  const fail = "😭 😱 Assertation Failed: ";
-  if (actual === expected) {
-    console.log(`${pass} ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} ${actual} !== ${expected}`);
-  }
-};
-
-
-/// this f(x) counts all letters in a string
-// spaces (results[" "]) are counted, but as per compass instructions are removed from final count
+// this f(x) counts all letters in a string
+// spaces (results[" "]) are counted, but as per project instructions are removed from final count
 
 const countLetters = function(sentence) {
   let results = {};
@@ -26,22 +13,6 @@ const countLetters = function(sentence) {
   return results;
 };
 
-// test code
+//
 
-const words = "lighthouse in the house";
-const wordsResult = countLetters(words, {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-});
-console.log(countLetters(words));
-assertEqual(wordsResult["h"], 4);
-assertEqual(wordsResult["n"], 1);
-assertEqual(wordsResult["z"], undefined);
+module.exports = countLetters;
